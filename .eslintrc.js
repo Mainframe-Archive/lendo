b46631137d386a86f7ae1d5b2c4f644a7ae4410c
Robson Silva junior <robsonsilvajunior@Robsons-MacBook-Pro.local>
2019-06-10 15:46:24 -0300

    all initial packages added

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['mainframe', 'mainframe/jest', 'mainframe/react-native-web'],
+  settings: {
+    react: {
+      version: '16.8.6',
+      flowVersion: '0.100.0',
+    },
+  },
+}
