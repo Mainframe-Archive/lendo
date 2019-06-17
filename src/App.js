494c4bb8616ddd6df756cc53dbe8a21144cd21d2
Robsonsjre <robsonsjre@gmail.com>
2019-06-17 01:37:01 -0300

    Loan Creation working on MFOS

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import logo from './logo.svg'
 import MainframeSDK from '@mainframe/sdk'
 // import styled from 'styled-components'
 import { ethers } from 'ethers'
-// import { abi } from './abi'
+import Web3 from 'web3'
+import { abi } from './abi'
 import './App.css'
 
-const abi = '';
-const provider = ethers.getDefaultProvider('kovan')
-const address = '0xFB23eC4FA01e228c2da6a1F3563F4Ba71Ffb1604'
-const privateKey =
-  '0x218CB65C69887CA18EAED478FA319BBE579DDB25759E23CA959BFE3D4BA6E51B'
+// const provider = ethers.getDefaultProvider('kovan')
+const contractAddress = '0x9D6427182e601Ba96eECcc28872aa93f90C58762'
+const privateKey = '0x218CB65C69887CA18EAED478FA319BBE579DDB25759E23CA959BFE3D4BA6E51B'
 
-const wallet = new ethers.Wallet(privateKey, provider)
+// const wallet = new ethers.Wallet(privateKey, provider)
 // const contract = new ethers.Contract(address, abi, wallet)
-
-// var sendPromise = contract.setValue('Hello World')
-
-
 
 // const Container = styled.View`
 //   flex: 1;
@@ -28,38 +23,67 @@ const wallet = new ethers.Wallet(privateKey, provider)
 // `;
 
 class App extends Component {
+
   constructor() {
     super()
-    // this.sdk = new MainframeSDK()
+
     this.state = {
+      sdk: new MainframeSDK(),
       contacts: [],
       loans: [],
       showNewLoan: false,
+      selectedContact: '',
+      amount: 0
     }
+
+    this.state.ethers = new ethers.providers.Web3Provider(this.state.sdk.ethereum.web3Provider)
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     //readSmartContract function
+    const contacts = await this.state.sdk.contacts.selectContacts()
+    const ownAccount = await this.state.sdk.ethereum.getDefaultAccount()
+
+    const wallet = new ethers.Wallet(privateKey, this.state.ethers)
+    const contract = new ethers.Contract(contractAddress, abi, wallet)
+
+    //change the way contacts array is made
+    const newContacts = contacts.map(contact => ({ ethAddress: contact.data.profile.ethAddress, name: contact.data.profile.name }))
+    console.log('ownAccount', ownAccount)
+
+    console.log('contract', contract)
+    const loans = await contract.getLoanAtAddress(ownAccount)
+    console.log('loans', loans[3])
+    this.setState({ loans, contacts: newContacts })
   }
 
   newLoan = () => {
     this.setState({ showNewLoan: true })
   }
 
-  createNewLoanContract = (a, b) => {
+  createNewLoanContract = async (a, b) => {
     a.preventDefault()
     console.log('call smart contract')
-    // const sendPromise = contract.setValue('newLoan')
+    console.log('this.state', this.state.selectedContact)
+    const wallet = new ethers.Wallet(privateKey, this.state.ethers)
+    const contract = new ethers.Contract(contractAddress, abi, wallet)
 
-    // sendPromise.then(function(transaction) {
-    //   console.log(transaction)
-    // })
+    console.log('amount', this.state.amount)
+    console.log('selectedContact', this.state.selectedContact)
+    const writeLoan = await contract.requestLoanRob(this.state.selectedContact, this.state.amount)
+    console.log('writeLoan', writeLoan)
+  }
+
+  selectContact = (option) => {
+    console.log('option', option.target.value)
+    this.setState({selectedContact: option.target.value})
   }
 
   render() {
-    // console.log('abi', abi)
+    console.log('this.state', this.state)
     return (
       <div className="App">
+        <h1>{this.state.loanStatus === 'completed' ? "You have a new Loan" : ''}</h1>
         <h1>My Loans</h1>
         {this.state.loans.map(loan => (
           <div>loan</div>
@@ -67,15 +91,18 @@ class App extends Component {
         {this.state.showNewLoan ? (
           <form style={{ justifyContent: 'center' }}>
             <div className="form-container">
-              <div className="row-item">
-                <div>Borrower Address</div>
-                <input type="text" />
+              <div className="row-item" style={{marginTop: '15px'}}>
+                <div>Lender</div>
+                <select value={this.state.selectedContact} onChange={this.selectContact}>
+                <option key={'None'} value={''}>None</option>
+                  {this.state.contacts.map(contact => <option key={contact.name} value={contact.ethAddress}>{contact.name}</option>)}
+                </select>
               </div>
               <div className="row-item">
                 <div>Amount</div>
-                <input label="Amount" type="text" />
+                <input label="Amount" type="text" value={this.state.amount} onChange={(e) => this.setState({amount: e.target.value})} />
               </div>
-              <button onClick={this.callSmartContract}> Send Invite </button>
+              <button onClick={this.createNewLoanContract}> Send Loan </button>
             </div>
           </form>
         ) : (
---
+++ src/abi.js
@@ -0,0 +1,281 @@
+export const abi = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      },
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "loansByBorrower",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      },
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "loansByLender",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "LoanStatus",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "totalLoanCount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "loans",
+    "outputs": [
+      {
+        "name": "swarmHash",
+        "type": "string"
+      },
+      {
+        "name": "lender",
+        "type": "address"
+      },
+      {
+        "name": "borrower",
+        "type": "address"
+      },
+      {
+        "name": "amount",
+        "type": "uint256"
+      },
+      {
+        "name": "status",
+        "type": "uint8"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "loansByBorrowerRob",
+    "outputs": [
+      {
+        "name": "swarmHash",
+        "type": "string"
+      },
+      {
+        "name": "lender",
+        "type": "address"
+      },
+      {
+        "name": "borrower",
+        "type": "address"
+      },
+      {
+        "name": "amount",
+        "type": "uint256"
+      },
+      {
+        "name": "status",
+        "type": "uint8"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "borrower",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "lender",
+        "type": "address"
+      }
+    ],
+    "name": "LoanRequested",
+    "type": "event"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "initialize",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "borrowerAddress",
+        "type": "address"
+      }
+    ],
+    "name": "getLoanAtAddress",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      },
+      {
+        "name": "",
+        "type": "address"
+      },
+      {
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "name": "",
+        "type": "string"
+      },
+      {
+        "name": "armor",
+        "type": "uint8"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "lender",
+        "type": "address"
+      },
+      {
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "requestLoan",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "lender",
+        "type": "address"
+      },
+      {
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "requestLoanRob",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "approveLoan",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "payDebt",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
