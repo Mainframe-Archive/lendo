4c1e5eaa644bf15932da04a20b2153751f7ee7bb
Guilherme Viana <ggviana@hotmail.com.br>
2019-06-20 15:03:15 -0300

    Added default Layout component

---
+++ packages/dapp/src/ui/Layouts/default.js
@@ -0,0 +1,28 @@
+// @flow
+import React, { type Node } from 'react'
+import styled from 'styled-components'
+import Header from 'ui/Header'
+
+const Main = styled.main`
+  flex: 1;
+  
+  .layout-body {
+    padding: 32px 35px;
+  }
+`
+
+type Props = {
+  title: Node,
+  children: Node
+}
+
+export default function DefaultLayout ({ children, title }: Props) {
+  return (
+    <Main>
+      <Header title={title}><br/></Header>
+      <div className="layout-body">
+        {children}
+      </div>
+    </Main>
+  )
+}
