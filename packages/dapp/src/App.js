4bb392a1b26f293138036e0ba7f1014bc03bbb39
Guilherme Viana <ggviana@hotmail.com.br>
2019-06-24 17:49:41 -0300

    Removing Google authorization

--- packages/dapp/src/App.js
+++ packages/dapp/src/App.js
@@ -6,38 +6,10 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom'
-
-import { createGlobalStyle } from 'styled-components'
-
+import GlobalStyles from 'styles'
 import Sidebar from 'ui/Sidebar'
-
 import { Dashboard, Loaned, Borrowed, Requests, NewLoanSetup } from 'pages'
 
-import { defaultFontFamily, defaultFontSize, defaultTextColor } from 'theme'
-
-const GlobalStyles = createGlobalStyle`
-  html {
-    font-size: ${defaultFontSize};
-  }
-
-  body {
-    background-color: white;
-    margin: 0;
-    font-family: ${defaultFontFamily};
-    color: ${defaultTextColor};
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  #root {
-    display: flex;
-    height: 100vh;
-  }
-`
 export default function App() {
   return (
     <Router>
